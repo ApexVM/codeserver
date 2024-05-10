86de09a1d03461acb79909df8a2dac4ae51fd266
Win8.1VMUser <78744628+Win81VMUser@users.noreply.github.com>
2024-05-10 22:25:34 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,2 +1,13 @@
 # Start a server on the VM!
 UNDER_DEVELOPMENT <3
+
+
+# Sl ur being hugged lel ill do my eork u finish
+# check if same for linux native
+sudo apt-get install -y build-essential pkg-config python3 nodejs npm python-is-python3
+npm config set python python3
+npm install --global code-server
+code-server
+# Now visit http://127.0.0.1:8080. Your password is in ~/.config/code-server/config.yaml
+
+# sl now finish :3
