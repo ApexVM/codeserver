3c29df412d396491eff6bb796cd4cd973e393b16
Win8.1VMUser <78744628+Win81VMUser@users.noreply.github.com>
2024-05-11 12:19:09 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -3,11 +3,10 @@ UNDER_DEVELOPMENT <3
 
 
 # Sl ur being hugged lel ill do my eork u finish
-# check if same for linux native
-sudo apt-get install -y build-essential pkg-config python3 nodejs npm python-is-python3
-npm config set python python3
-npm install --global code-server
-code-server
-# Now visit http://127.0.0.1:8080. Your password is in ~/.config/code-server/config.yaml
 
+# check for ApexVM CDN for installer (now using this)
+curl -fsSL https://code-server.dev/install.sh | sh
+clear
+# setup ApexVM RunCode domain systems
+# sl u do it
 # sl now finish :3
