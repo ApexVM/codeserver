b1d9ef62c7fc30b7baecff72877c3b3e4224ad9a
Slara <162091285+slara6804@users.noreply.github.com>
2024-05-12 13:13:16 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,13 +1,18 @@
 # Start a server on the VM!
 UNDER_DEVELOPMENT <3
 
+CDN_URL="https://apexvm.snares-gar.ts.net/cdn/"
 
-# Sl ur being hugged lel ill do my eork u finish
 
-# check for ApexVM CDN for installer (now using this)
+# Download installer
 curl -fsSL https://code-server.dev/install.sh | sh
-# curl -fsSL https://apexvm.snares-gar.ts.net/cdn/files/install.sh | sh
+# curl -fsSL https://apexvm.snares-gar.ts.net/cdn/.avm/scripts/install.sh | sh
 clear
-# setup ApexVM RunCode domain systems
-# sl u do it
-# sl now finish :3
+# Setup ApexVM RunCode domain systems
+# Nuh uh, domains are your stuff
+
+# Set up desktop environment
+curl -fsSL CDN_URL/.avm/scripts/setupde.sh | sh
+# Patch AVM script
+mkdir ~/.avm
+curl -fsSL CDN_URL/.avm/scripts/setupavm.sh | sh
