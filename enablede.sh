6a84d5a57d12e6410d0f75191348591394bc4256
Slara <162091285+slara6804@users.noreply.github.com>
2024-05-12 13:03:15 +0200

    Update enablede.sh

--- enablede.sh
+++ enablede.sh
@@ -1,2 +1,17 @@
 # Enable VNC and Desktop Environment for your RunCode instance
-SOON <3
+
+# Install XFCE4
+sudo apt-get install -y xfce4 xfce4-session tigervnc-standalone-server
+# Set up VNC and start session
+vncserver :1 -xstartup /usr/bin/startxfce4 -localhost no -SecurityTypes none --I-KNOW-THIS-IS-INSECURE -geometry 1024x768
+# Patch forwader
+# vmuser, please patch it using your scripts
+# Push avm scripts
+echo "vncserver :1 -xstartup /usr/bin/startxfce4 -localhost no -SecurityTypes none --I-KNOW-THIS-IS-INSECURE -geometry 1024x768" > .avm/vnc.avm
+echo "vncserver -kill :1" > .avm/killvnc.avm
+# Patch AVM manager
+curl CDN_URL/.avm/runcode/avm.cxx -o avm.cxx
+g++ avm.cxx -o avm
+sudo mv avm /usr/local/bin/avm && sudo chmod 777 /usr/local/bin/avm
+# Clean terminal and show system config
+clear && bash .avm/info.avm
